63a6b400a6a55d11f0a856991269bab9678ec703
Karthikeya <karthikeyagaddam257@gmail.com>
2026-01-24 20:16:19 +0530

    title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,3 @@
-// import { NavLink } from "react-router-dom";
-
-// export default function Navbar() {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-//       <span className="navbar-brand fw-bold">🥛 Milk Note</span>
-
-//       {/* Mobile toggle */}
-//       <button
-//         className="navbar-toggler"
-//         type="button"
-//         data-bs-toggle="collapse"
-//         data-bs-target="#milkNavbar"
-//       >
-//         <span className="navbar-toggler-icon"></span>
-//       </button>
-
-//       <div className="collapse navbar-collapse" id="milkNavbar">
-//         <div className="navbar-nav ms-auto gap-2">
-//           <NavLink
-//             to="/"
-//             className={({ isActive }) =>
-//               `btn btn-sm ${
-//                 isActive ? "btn-light" : "btn-outline-light"
-//               }`
-//             }
-//           >
-//             Entry
-//           </NavLink>
-
-//           <NavLink
-//             to="/reports"
-//             className={({ isActive }) =>
-//               `btn btn-sm ${
-//                 isActive ? "btn-light" : "btn-outline-light"
-//               }`
-//             }
-//           >
-//             Reports
-//           </NavLink>
-
-//           <NavLink
-//             to="/charts"
-//             className={({ isActive }) =>
-//               `btn btn-sm ${
-//                 isActive ? "btn-light" : "btn-outline-light"
-//               }`
-//             }
-//           >
-//             Charts
-//           </NavLink>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
 
 import { NavLink } from "react-router-dom";
 
@@ -68,7 +11,7 @@ export default function Navbar() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-      <span className="navbar-brand fw-bold">🥛 Milk Manager</span>
+      <span className="navbar-brand fw-bold">🥛 Milk Note</span>
 
       {/* Toggle button */}
       <button
