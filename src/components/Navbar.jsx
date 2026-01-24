197a30f7133a5f5f1fa7bfe3c4b49d730c0e63fe
Karthikeya <karthikeyagaddam257@gmail.com>
2026-01-24 19:58:40 +0530

    Logo added,Tilte change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-      <span className="navbar-brand fw-bold">🥛 Milk Manager</span>
+      <span className="navbar-brand fw-bold">🥛 Milk Note</span>
 
       {/* Mobile toggle */}
       <button
