36072d2d7cd78772f013b08d320b1a83bba7bef0
Karthikeya <karthikeyagaddam257@gmail.com>
2026-01-24 20:35:30 +0530

    Minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
 export default function Footer() {
     return (
       <footer
-        className="text-center text-muted py-3 mt-5"
-        style={{
-          fontSize: "0.85rem",
-          borderTop: "1px solid #eaeaea"
-        }}
-      >
-        🥛 Milk Manager · Designed & Developed by{" "}
-        <strong>Karthikeya & Team</strong> 💙
-      </footer>
+      className="text-center text-muted py-2 fixed-bottom bg-white"
+      style={{
+        fontSize: "0.85rem",
+        borderTop: "1px solid #eaeaea"
+      }}
+    >
+      🥛 Milk Manager · Designed & Developed by{" "}
+      <strong>Karthikeya & Team</strong> 💙
+    </footer>
+    
     );
   }
   
